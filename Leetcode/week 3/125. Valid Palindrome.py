5c6ca4bbcd0b103769773194bb3c596296a0db53
bogeoung <naturalbb360@gmail.com>
2021-01-19 22:53:47 +0900

    update

--- Leetcode/week 3/125. Valid Palindrome.py
+++ Leetcode/week 3/125. Valid Palindrome.py
@@ -7,8 +7,9 @@ class Solution:
         # 문자를 넣을 deque
         strings = deque()
 
+        # 소문자로 통일한 문장에서 영어, 한글만 strings 덱에 추가
         for i in s:
-            if s.isalnum() is True:
+            if i.isalnum(): # if i.isalnum() is True은 속도가 더 느림
                 strings.append(i)
         # 덱의 제일 앞과 뒤가 같은지 비교
         while len(strings) > 1: # 제일 앞과 뒤를 비교할 것이기 때문에 한개가 남으면 빠져나감
@@ -16,5 +17,14 @@ class Solution:
                 return False
         return True
 
-test = Solution()
-print(test.isPalindrome("race is a car"))
+        ''' Another Answer
+        s = s.lower() 
+        s = re.sub('^a-z0-9]','',s) 
+        return s = s[::-1]
+        
+        1. 소문자로 바꿈
+        2. 정규표현식을 통해 불필요한 문자를 제거. 
+           ^은 not을 의미 -> a-z 혹은 0-9가 아니면 공백으로 치환한다는 의미
+        3. s와 s[::-1]가 같은지를 비교하여 Ture 혹은 False리턴
+           이때 s[::-1]은 문자열 슬라이스의 한 기능으로 문자열을 뒤집는다.
+        '''
--- Leetcode/week 3/344. Reverse String.py
+++ Leetcode/week 3/344. Reverse String.py
@@ -1,10 +1,25 @@
 class Solution:
     def reverseString(self, s) -> None:
+        # 반복할 횟수를 정하기 위해 전체길이의 2를 나눈 몫을 구함
         time = len(s)/2
+        # 바꿀 문자열의 위치를 지정할 변수 count 선언
         count = 0
         while time >= 1 :
-            time -= 1
-            temp = s[count]
-            s[count] = s[-(count+1)]
+            time -= 1 # 무한루프를 막기 위해 time 감소
+            temp = s[count] # temp라는 임시 변수에 s[count]값을 저장 후, 값을 교환함
+            s[count] = s[-(count+1)] # 파이썬에서는 s[count], s[-(count+1)] = s[-(count+1), s[count]로 사용 가능하다고 함.]
             s[-(count+1)] = temp
             count += 1
+
+        ''' Another Answer 1
+        def reverseString(self, s:List[str]) -> None:
+        left, right = 0, len(s) -1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        '''
+        ''' Another Answer 2
+        def reverseString(self, s:List[str]) -> None:
+            s.reverse() # 파이썬 리스트의 메소드 reverse()를 이용
+        '''
