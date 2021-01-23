b1fbeb73702bfae0f4dcfa249d5297ca5dd2ae7f
bogeoung <naturalbb360@gmail.com>
2021-01-24 00:28:34 +0900

    Week 5

--- Leetcode/week 5/21. Merge Two Sorted Lists.py
+++ Leetcode/week 5/21. Merge Two Sorted Lists.py
@@ -7,18 +7,37 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        templist = ListNode()
-
         if l1 is None:
             return l2
         if l2 is None:
             return l1
 
+        if l1.val < l2.val:
+            head = l1.val
+        else:
+            head = l2.val
+
+        templist = ListNode(head)
+
         while True:
+            print("l1.val", l1.val, "l2.val", l2.val)
+
             if l1.val < l2.val:
                 templist.next = ListNode(l1.val)
-                l1 = l1.next
+                if l1.next is not None:
+                    l1 = l1.next
+                    templist = templist.next
+                else:
+                    break
 
             else:
                 templist.next = ListNode(l2.val)
-                l2 = l2.next
+                if l2.next is not None:
+                    l2 = l2.next
+                    templist = templist.next
+                else:
+                    break
+
+            print("templist", templist)
+
+        return templist
