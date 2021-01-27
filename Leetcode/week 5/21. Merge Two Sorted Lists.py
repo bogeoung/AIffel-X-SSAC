01812824df630d773dc05e8273a721db94a04f11
bogeoung <naturalbb360@gmail.com>
2021-01-27 18:27:40 +0900

    Week 5

--- Leetcode/week 5/21. Merge Two Sorted Lists.py
+++ Leetcode/week 5/21. Merge Two Sorted Lists.py
@@ -23,21 +23,22 @@ class Solution:
             print("l1.val", l1.val, "l2.val", l2.val)
 
             if l1.val < l2.val:
-                templist.next = ListNode(l1.val)
+                templist.next = l1
+                # next는 다음 연결리스트의 주소를 가리키는 것임.
+                # templist.next = ListNode(l1.val)
+                # 따라서 ListNode(l1.val)이 아닌 l1을 넣어야 함.
                 if l1.next is not None:
                     l1 = l1.next
-                    templist = templist.next
                 else:
                     break
 
             else:
-                templist.next = ListNode(l2.val)
+                templist.next = l2
                 if l2.next is not None:
                     l2 = l2.next
-                    templist = templist.next
                 else:
                     break
-
+            templist = templist.next
             print("templist", templist)
 
         return templist
--- Leetcode/week 5/24. Swap Nodes in Pairs.py
+++ Leetcode/week 5/24. Swap Nodes in Pairs.py
@@ -7,7 +7,7 @@ class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
         node = head
         while node and node.next:
-            node.val , node.next.val = node.next.val, node.val
+            node.val, node.next.val = node.next.val, node.val
             node = node.next.next
         return head
 
