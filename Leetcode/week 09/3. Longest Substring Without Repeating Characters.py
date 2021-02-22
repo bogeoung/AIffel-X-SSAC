0ac450c09a65a232a7a6244e8ff782f4d33bbb77
bogeoung <naturalbb360@gmail.com>
2021-02-23 01:58:11 +0900

    Week 9

--- Leetcode/week 09/3. Longest Substring Without Repeating Characters.py
+++ Leetcode/week 09/3. Longest Substring Without Repeating Characters.py
@@ -2,7 +2,7 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         hashmap = {}
         count = 0
-
+        
         # try 1
         # 앞에 연속적인 문자가 나온 경우 찾지를 못함.
         # for char in s:
@@ -12,12 +12,12 @@ class Solution:
         #     else:
         #         count = 0
         #         hashmap = {}
-
+                
         # try 2
-        
+
 
         print(count)
         return count
-
+    
 num = Solution()
 num.lengthOfLongestSubstring("abcabcbb")
