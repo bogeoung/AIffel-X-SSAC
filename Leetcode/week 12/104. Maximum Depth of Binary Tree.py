eef42edf1c64ec33dc7d8a9cf0deb66f56197f77
bogeoung <naturalbb360@gmail.com>
2021-03-17 04:13:44 +0900

    Week 12 - tree

--- Leetcode/week 12/104. Maximum Depth of Binary Tree.py
+++ Leetcode/week 12/104. Maximum Depth of Binary Tree.py
@@ -6,5 +6,31 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+
+        # 큐 생성 후 root를 원소로 넣음.
+        queue = collections.deque([root])
         depth = 0
 
+        while queue:
+            depth +=1
+            # 부모노드의 길이만큼 반복함.
+            for _ in range(len(queue)):
+                # queue.popright()를 넣어보니 존재하지 않는 메소드라고 떴음.
+                cur_root = queue.popleft()
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
+
+        return depth
+#
+# def maxDepth(slef, root:TreeNode) -> int:
+#     BFS는 큐를 사용하기 때문에 큐 선언. (DFS는 스택 사용)
+#     queue = collections.deque([root])
+#     depth = 0
+#
+#     while queue:
+#
+#     return depth
