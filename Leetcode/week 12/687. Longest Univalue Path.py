dc5e67f5b5747de5f5a51a6e767ec5f69ba205d0
bogeoung <naturalbb360@gmail.com>
2021-03-17 12:24:27 +0900

    Week 12 - tree

---
+++ Leetcode/week 12/687. Longest Univalue Path.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    result : int = 0
+    def longestUnivaluePath(self, root: TreeNode) -> int:
+        def dfs(node: TreeNode) -> int:
+            if not node:
+                return -1
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if node.left and node.left.val == node.val:
+                left +=1
+            else:
+                left = 0
+
+            if node.right and node.right.val == node.val:
+                right += 1
+            else:
+                right = 0
+
+            self.result = max(self.result, left + right)
+            return max(left, right)
+
+        dfs(root)
+        return self.result
